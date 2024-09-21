e413798a623b09d53aff64530dc833741e59e1f4
Nikolai Chaourov <nik.chaourov@gmail.com>
2024-09-22 09:55:08 +1000

    scheduled job

--- bin/scheduled-workflow-cdk.ts
+++ bin/scheduled-workflow-cdk.ts
@@ -1,21 +1,11 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { ScheduledWorkflowCdkStack } from '../lib/scheduled-workflow-cdk-stack';
+import {ScheduledWorkflowStack} from '../lib/scheduled-workflow-cdk-stack';
 
 const app = new cdk.App();
-new ScheduledWorkflowCdkStack(app, 'ScheduledWorkflowCdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const env = { account: "AWS_ACCOUNT_ID", region: "AWS_REGION" };
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new ScheduledWorkflowStack(app, 'ScheduledWorkflowStack', {
+    env
 });
--- lib/scheduled-workflow-cdk-stack.ts
+++ lib/scheduled-workflow-cdk-stack.ts
@@ -1,16 +1,51 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
+import * as sfnTasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import * as path from 'path';
+import {DefinitionBody, WaitTime} from "aws-cdk-lib/aws-stepfunctions";
+import {Duration} from "aws-cdk-lib";
 
-export class ScheduledWorkflowCdkStack extends cdk.Stack {
+export class ScheduledWorkflowStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Define the Lambda function you'll run in your first step
+    // A lambda can be anything you want, for more flexibility try deploying a lambda from a docker image in ECR!
+    const lambdaFunction = new lambda.Function(this, 'MyLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/happy-lambda')),
+      handler: 'index.handler',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ScheduledWorkflowCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Define a simple Step Function task (replace with actual logic)
+    const task = new sfnTasks.LambdaInvoke(this, 'StepFunctionHappyTask', {
+      lambdaFunction
+    });
+
+    const wait = new sfn.Wait(this, 'WaitTask', {
+      time: WaitTime.duration(Duration.seconds(5)),
+    });
+
+    // Feel free to chain more!
+    const steps = sfn.Chain.start(wait).next(task);
+
+    // Define the Step Function state machine
+    const definitionBody = DefinitionBody.fromChainable(steps);
+
+    const stateMachine = new sfn.StateMachine(this, 'MyStateMachine', {
+      definitionBody,
+    });
+
+    // Define a CloudWatch Event rule to trigger the Lambda every 5 minutes
+    const rule = new events.Rule(this, 'ScheduleRule', {
+      schedule: events.Schedule.rate(cdk.Duration.minutes(5)),  // Schedule every 5 minutes
+    });
+
+    // Add the Step Function as the target for the rule
+    rule.addTarget(new targets.SfnStateMachine(stateMachine));
   }
 }
